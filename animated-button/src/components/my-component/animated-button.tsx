0a0e357d7283de0a64569b1c4fba71e0b01658f8
Willy Alers <willy.alers@allheart.net>
2022-02-04 16:32:50 -0800

    Removed unneeded boilerplate code.

---
+++ animated-button/src/components/my-component/animated-button.tsx
@@ -0,0 +1,14 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'animated-button',
+  styleUrl: 'animated-button.css',
+  shadow: true,
+})
+export class AnimatedButton {
+  render() {
+    return <div>
+
+    </div>;
+  }
+}
